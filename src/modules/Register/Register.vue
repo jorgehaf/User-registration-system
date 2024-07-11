5f0a29f5178cf5846ca993aa30c781554ccc1ad9
Jorge Ferreira <jorge.ferreira@vitta.me>
2024-07-10 21:56:37 -0300

    Etapa de pessoa fisica

--- src/modules/Register/Register.vue
+++ src/modules/Register/Register.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="register">
     <div class="register-form">
-      <div class="register-head">
-        <p>Etapa <span class="stage">{{ stage }}</span> de 4</p>
-      </div>
-      <EmailStage v-if="stage === STAGES.EMAIL" @confirmEmailStage="confirmEmailStage"/>
+      <p>
+        Etapa <span class="stage">{{ stage }}</span> de 4
+      </p>
+
+      <EmailStage
+        v-if="stage === STAGES.EMAIL"
+        @confirmEmailStage="confirmEmailStage"
+      />
+      <PhysicalPersonStage
+        v-if="stage === STAGES.PHYSICAL_PERSON"
+        @backPhysicalPersonStage="backPhysicalPersonStage"
+        @confirmPhysicalPersonStage="confirmPhysicalPersonStage"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import { STAGES } from '../../enums/registrationStages.js'
-import EmailStage from './components/EmailStage/EmailStage.vue'
+import { STAGES } from "../../enums/registrationStages.js";
+import EmailStage from "./components/EmailStage/EmailStage.vue";
+import PhysicalPersonStage from "./components/PhysicalPersonStage/PhysicalPersonStage.vue";
 
 const stage = ref(1);
 
@@ -23,8 +33,20 @@ const confirmEmailStage = (data) => {
     stage.value += 1;
   }
 };
+
+const backPhysicalPersonStage = (data) => {
+  console.log("confirmPhysicalPersonStage: ", data);
+
+  stage.value = STAGES.EMAIL;
+};
+
+const confirmPhysicalPersonStage = (data) => {
+  console.log("confirmPhysicalPersonStage: ", data);
+
+  stage.value = STAGES.ACCESS_PASSWORD;
+};
 </script>
 
 <style scoped>
-@import './Register.scss';
+@import "./Register.scss";
 </style>
---
+++ src/modules/Register/components/PhysicalPersonStage/PhysicalPersonStage.vue
@@ -0,0 +1,85 @@
+<template>
+  <form
+    @submit.prevent="
+      $emit('confirmPhysicalPersonStage', { nome, cpf, date, number })
+    "
+  >
+    <p id="form-title">Pessoa Física(a)</p>
+
+    <label for="nome">Nome</label>
+    <input type="text" id="nome" v-model="nome" required />
+
+    <label for="cpf">CPF</label>
+    <input
+      type="text"
+      id="cpf"
+      v-model="cpf"
+      @input="formatCpf"
+      placeholder="___.___.___-__"
+      required
+    />
+
+    <label for="date">Data de nascimento</label>
+    <input id="date" type="date" v-model="date" />
+
+    <label for="number">Telefone</label>
+    <input
+      id="number"
+      type="tel"
+      v-model="number"
+      @input="formatNumber"
+      placeholder="(__)_____-____"
+      required
+    />
+
+    <div class="buttons-group">
+      <button @click="$emit('backPhysicalPersonStage')">Voltar</button>
+      <button type="submit">Continuar</button>
+    </div>
+  </form>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const nome = ref("");
+const cpf = ref("");
+const date = ref("");
+const number = ref("");
+
+const formatNumber = (event) => {
+  let input = event.target.value.replace(/\D/g, "");
+
+  if (input.length > 0) {
+    input = `(${input.substring(0, 2)}) ${input.substring(
+      2,
+      7
+    )}-${input.substring(7, 11)}`;
+  }
+
+  number.value = input;
+};
+
+const formatCpf = (event) => {
+  let input = event.target.value.replace(/\D/g, "");
+
+  if (input.length <= 3) {
+    input = input.replace(/^(\d{0,3})/, "$1");
+  } else if (input.length <= 6) {
+    input = input.replace(/^(\d{0,3})(\d{0,3})/, "$1.$2");
+  } else if (input.length <= 9) {
+    input = input.replace(/^(\d{0,3})(\d{0,3})(\d{0,3})/, "$1.$2.$3");
+  } else if (input.length <= 11) {
+    input = input.replace(
+      /^(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,2})/,
+      "$1.$2.$3-$4"
+    );
+  }
+
+  cpf.value = input;
+};
+</script>
+
+<style scoped>
+@import "./PhysicalPersonStage.scss";
+</style>
